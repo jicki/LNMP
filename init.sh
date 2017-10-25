9205c44f05f8962b5619606e8994b7c2a8c0b6e4
jicki <jicki@users.noreply.github.com>
2017-10-25 12:13:04 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+mkdir -p ./data/mysql
+mkdir -p ./data/nginx/htdocs
+mkdir -p ./data/svn
+mkdir -p ./data/gogs
+mkdir -p ./data/nexus
+
+
 chown -R systemd-bus-proxy:input ./data/mysql
 chown -R 200:200 ./data/nexus
